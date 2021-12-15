f75685181b2a87bbb99368513bb116e0fd26e2cb
Horváth Dániel <admin@hdani1337.hu>
2021-12-15 13:52:30 +0100

    Első félév

---
+++ gyakorlas/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("kivon").click({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+
+    $("osszead").click({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+
+    $("szoroz").click({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+
+    $("oszt").click({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+  });
---
+++ product/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("address").change({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+
+    $("gomb").click({function(){
+        alert("Hello! I am an alert box!!");
+    }});
+  });
